5b8183d8fa45761843224b00f85d4447e976bffe
Marcelo Leal <andradefj13@hotmail.com>
2023-12-03 15:52:32 -0300

    update
    criação do layout(header) e idealização do registry

--- react-user-registration/src/layout.tsx
+++ react-user-registration/src/layout.tsx
@@ -1,14 +1,30 @@
 import { Outlet } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 
 export function Layout() {
     return (
         <>
             <header>
-                
+                <Navbar expand="lg" bg="dark" data-bs-theme="dark">
+                    <Container>
+                        <Navbar.Brand href="/">React User-Registration</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="/">Início</Nav.Link>
+                                <Nav.Link href="candidates">Candidatos</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
             </header>
-            <div>
+            <>
                 <Outlet />
-            </div>
+            </>
             <footer>
 
             </footer>
